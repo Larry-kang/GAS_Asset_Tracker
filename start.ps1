c3366969e367141c89bbf1d851f12eb2ef4dcd16
Larry Kang <akqazk@gmail.com>
2025-12-27 15:10:19 +0800

    fix: Remove start.ps1 from repo and fix typo

--- start.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = 'Stop'
-Write-Host "? Pushing code to Google Apps Script..."
-clasp push -f
-Write-Host "? Code Pushed! Please go to GAS Editor run 'getBinanceBalance' to verify."
